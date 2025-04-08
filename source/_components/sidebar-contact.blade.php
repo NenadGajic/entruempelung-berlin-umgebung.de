0f7b1bbc05a97cc35adac8f861272a50c413230e
Nenad Gajic <nenad_gajic@outlook.com>
2025-04-09 00:30:29 +0200

    Extract content to separate component

---
+++ source/_components/sidebar-contact.blade.php
@@ -0,0 +1,12 @@
+<div class="all__sidebar-item-help mb-25">
+    <a href="/">
+        <img src="/assets/img/logo-w.png" alt="Logo">
+    </a>
+
+    <span>Können wir Ihnen helfen?</span>
+
+    <h4><a href="tel(415)755-7890">(415) 755-7890</a></h4>
+    <a class="default_button" href="/anfrage">
+        Termin vereinbaren<i class="flaticon-right-up"></i>
+    </a>
+</div>
--- source/entruempelung.blade.php
+++ source/entruempelung.blade.php
@@ -37,12 +37,7 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="all__sidebar-item-help mb-25">
-                        <a href="/"><img src="/assets/img/logo-w.png" alt=""></a>
-                        <span>Können wir Ihnen helfen?</span>
-                        <h4><a href="tel(415)755-7890">(415) 755-7890</a></h4>
-                        <a class="default_button" href="/anfrage">Termin vereinbaren<i class="flaticon-right-up"></i></a>
-                    </div>
+                    <x-sidebar-contact />
                 </div>
             </div>
             <div class="col-lg-8">
