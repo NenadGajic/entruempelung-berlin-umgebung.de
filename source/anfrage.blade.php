a33cdf263edfff18140d20639d2d5620804425e9
Nenad Gajic <nenad_gajic@outlook.com>
2025-04-08 15:37:33 +0200

    Update anfrage.blade.php

--- source/anfrage.blade.php
+++ source/anfrage.blade.php
@@ -23,76 +23,82 @@
                 <div class="col-xl-12">
                     <form action="#">
                         <div class="row">
-                            <div class="col-md-6 mt-25">
+{{--                            <div class="col-md-4 mt-25">--}}
+{{--                                <div class="request__quote-item">--}}
+{{--                                    <label>Anrede <span> *</span></label>--}}
+{{--                                    <select name="anrede">--}}
+{{--                                        <option value="Herr">Herr</option>--}}
+{{--                                        <option value="Frau">Frau</option>--}}
+{{--                                    </select>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+
+                            <div class="col-md-128 mt-25">
                                 <div class="request__quote-item">
-                                    <label>First Name<span> *</span></label>
-                                    <input type="text" name="name" placeholder="Michael" required>
+                                    <label>Vollständiger Name<span> *</span></label>
+                                    <input type="text" name="name" placeholder="Max Mustermann" required>
                                 </div>
                             </div>
+
                             <div class="col-md-6 mt-25">
                                 <div class="request__quote-item">
-                                    <label>Last Name</label>
-                                    <input type="text" placeholder="Brown">
+                                    <label>E-Mail Adresse<span> *</span></label>
+                                    <input type="email" name="email" placeholder="beispiel@email.com" required>
                                 </div>
                             </div>
+
                             <div class="col-md-6 mt-25">
                                 <div class="request__quote-item">
-                                    <label>Email Address<span> *</span></label>
-                                    <input type="email" placeholder="michaelbrown@example.com" required>
+                                    <label>Telefonnummer<span> *</span></label>
+                                    <input type="text" name="nummer" placeholder="+49 123456789" required>
                                 </div>
                             </div>
+
                             <div class="col-md-6 mt-25">
                                 <div class="request__quote-item">
-                                    <label>Number<span> *</span></label>
-                                    <input type="text" placeholder="+44 7911 123456" required>
+                                    <label>Postleitzahl und Ortschaft<span> *</span></label>
+                                    <input type="text" name="plz" placeholder="z.B.: 10115 Berlin" required>
                                 </div>
                             </div>
+
                             <div class="col-md-6 mt-25">
                                 <div class="request__quote-item">
-                                    <label>Company/Organization<span> *</span></label>
-                                    <input type="text" placeholder="Tesla" required>
+                                    <label>Straße und Hausnummer<span> *</span></label>
+                                    <input type="text" name="strasse" placeholder="Musterstraße 1/20" required>
                                 </div>
                             </div>
-                            <div class="col-md-6 mt-25">
-                                <div class="request__quote-item">
-                                    <label>Website<span> *</span></label>
-                                    <input type="url" placeholder="https://tesla.com" required>
-                                </div>
-                            </div>
+
                             <div class="col-md-12 mt-25">
-                                <p class="mb-10">What services can we provide you?<span> *</span></p>
+                                <p class="mb-10">Welche Leistungen benötigen Sie von uns?<span> *</span></p>
                                 <div class="row">
                                     <div class="col-md-4">
                                         <div class="request__quote-services">
-                                            <label><input class="mr-10" type="checkbox">Optimization (SEO)</label>
-                                            <label><input class="mr-10" type="checkbox">Web Design</label>
-                                            <label><input class="mr-10" type="checkbox">Web Hosting / Maintenance</label>
+                                            <label><input class="mr-10" type="checkbox">Entrümpelung</label>
+                                            <label><input class="mr-10" type="checkbox">Entsorgung</label>
+                                            <label><input class="mr-10" type="checkbox">Büro- oder Haushaltsauflösung</label>
                                         </div>
                                     </div>
                                     <div class="col-md-4">
                                         <div class="request__quote-services">
-                                            <label><input class="mr-10" type="checkbox">Content Writing</label>
-                                            <label><input class="mr-10" type="checkbox">Search Engine Marketing</label>
-                                            <label><input class="mr-10" type="checkbox">Social Media</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="request__quote-services">
-                                            <label><input class="mr-10" type="checkbox">ADA Compliance</label>
-                                            <label><input class="mr-10" type="checkbox">Photography / Video</label>
-                                            <label><input class="mr-10" type="checkbox">Email Marketing</label>
+                                            <label><input class="mr-10" type="checkbox">Kleintransport</label>
+                                            <label><input class="mr-10" type="checkbox">Umzug</label>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
                             <div class="col-md-12 mt-25">
                                 <div class="request__quote-item">
-                                    <label>Message<span> *</span></label>
-                                    <textarea name="message" placeholder="I'm interested in your services. Please contact me."></textarea>
+                                    <label>Anmerkungen / Details<span> *</span></label>
+                                    <textarea name="message" placeholder="Zusätzliche Infos an uns"></textarea>
                                 </div>
                             </div>
+                            
                             <div class="col-lg-12">
-                                <button class="default_button mt-25" type="submit">Submit Now<i class="flaticon-right-up"></i></button>
+                                <button class="default_button mt-25" type="submit">
+                                    Anfrage Senden
+                                    <i class="flaticon-right-up"></i>
+                                </button>
                             </div>
                         </div>
                     </form>
