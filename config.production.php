a743671c2d083bf5e6bd7c1b0e276ec28f893fdd
Nenad Gajic <nenad_gajic@outlook.com>
2025-05-17 12:58:02 +0200

    Add sitemap generator

--- config.production.php
+++ config.production.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Str;
 
 return [
     'production' => true,
-//    'baseUrl' => 'https://www.entruempelung-berlin-umgebung.de',
-    'baseUrl' => 'https://entruempelung-berlin.netlify.app',
+    'baseUrl' => 'https://entruempelung-berlin-umgebung.de',
     'title' => 'Entrümpelung in Berlin und Umgebung',
     'description' => 'Wir sind Ihre Experten für schnelle und zuverlässige Entrümpelung in Berlin. Ob Möbel, Elektrogeräte oder großer Sperrmüll – wir entsorgen alles fachgerecht und umweltbewusst.',
     'active' => function ($page, $section) {
