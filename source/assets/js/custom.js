8a55b0da5231cc03d4339d4282126283c86e89d4
Nenad Gajic <nenad_gajic@outlook.com>
2026-02-14 16:02:50 +0100

    refactor(js): reorganize custom.js into modular behavior-preserving handlers

--- source/assets/js/custom.js
+++ source/assets/js/custom.js
@@ -1,123 +1,150 @@
 (function ($) {
     "use strict";
 
-    ///=============  * Mobile Menu  =============\\\
-    document.addEventListener("DOMContentLoaded", function() {
-        var mobileMenu = document.getElementById("mobilemenu");
-        var mainMenu = document.querySelector(".vertical_menu");
+    const SELECTORS = {
+        mobileMenu: '#mobilemenu',
+        verticalMenu: '.vertical_menu',
+        menuItemWithChildren: '.menu-item-has-children',
+        submenuToggle: '.menu-item-has-children > .submenu-toggle',
+        menuTrigger: '.menu__bar-trigger',
+        menuPopup: '.menu__bar-popup',
+        menuOverlay: '.menu__bar-popup-overlay',
+        slider: '.banner-slider',
+        serviceField: '#service',
+        conditionalGroup: '.conditional-group',
+        contactForm: '#contact-form',
+    };
+
+    const ALLOWED_SERVICES = ['entruempelung', 'entsorgung', 'aufloesung', 'umzug', 'transport'];
+
+    function initializeMobileMenuClone() {
+        const mobileMenu = document.querySelector(SELECTORS.mobileMenu);
+        const mainMenu = document.querySelector(SELECTORS.verticalMenu);
+
         if (!mobileMenu || !mainMenu || mainMenu.children.length) {
             return;
         }
-        var clonedMenu = mobileMenu.cloneNode(true);
-        clonedMenu.removeAttribute("id");
+
+        const clonedMenu = mobileMenu.cloneNode(true);
+        clonedMenu.removeAttribute('id');
         mainMenu.appendChild(clonedMenu);
-    });
-    jQuery(document).ready(function($) {
-        $('.vertical_menu ul li.menu-item-has-children').each(function() {
-            if (!$(this).children('.mobile-arrows').length) {
-                $(this).append('<span class="mobile-arrows far fa-plus" aria-hidden="true"></span>');
-            }
-        });
+    }
 
-        $(".vertical_menu .mobile-arrows").on("click",function() {
-            const parent = $(this).parent();
-            const submenu = parent.find('ul:first');
-            submenu.slideToggle(300);
-            $(this).toggleClass('is-open');
+    function initializeMobileMenuArrows() {
+        const $verticalMenu = $(SELECTORS.verticalMenu);
 
-            const isExpanded = $(this).hasClass('is-open');
-            parent.toggleClass('is-open', isExpanded);
-            parent.children('.submenu-toggle').attr('aria-expanded', String(isExpanded));
-        });
-    });
-
-    function submenuToggle() {
-        const submenuItems = $('.menu-item-has-children > .submenu-toggle');
-
-        if (!submenuItems.length) {
+        if (!$verticalMenu.length) {
             return;
         }
 
-        submenuItems.on('click', function(event) {
-            event.preventDefault();
-            const item = $(this).parent();
-            const isOpen = item.hasClass('is-open');
-
-            item.siblings('.menu-item-has-children').removeClass('is-open')
-                .children('.submenu-toggle')
-                .attr('aria-expanded', 'false');
-
-            item.toggleClass('is-open', !isOpen);
-            $(this).attr('aria-expanded', String(!isOpen));
-        });
-
-        $(document).on('click', function(event) {
-            if (!$(event.target).closest('.menu-item-has-children').length) {
-                $('.menu-item-has-children.is-open')
-                    .removeClass('is-open')
-                    .children('.submenu-toggle')
-                    .attr('aria-expanded', 'false');
+        $verticalMenu.find('ul li.menu-item-has-children').each(function () {
+            const $item = $(this);
+            if (!$item.children('.mobile-arrows').length) {
+                $item.append('<span class="mobile-arrows far fa-plus" aria-hidden="true"></span>');
             }
         });
 
-        $(document).on('keydown', function(event) {
+        $verticalMenu.on('click', '.mobile-arrows', function () {
+            const $arrow = $(this);
+            const $parent = $arrow.parent();
+            const $submenu = $parent.find('ul:first');
+
+            $submenu.slideToggle(300);
+            $arrow.toggleClass('is-open');
+
+            const isExpanded = $arrow.hasClass('is-open');
+            $parent.toggleClass('is-open', isExpanded);
+            $parent.children('.submenu-toggle').attr('aria-expanded', String(isExpanded));
+        });
+    }
+
+    function closeDesktopSubmenus() {
+        $(SELECTORS.menuItemWithChildren + '.is-open')
+            .removeClass('is-open')
+            .children('.submenu-toggle')
+            .attr('aria-expanded', 'false');
+    }
+
+    function initializeDesktopSubmenuToggle() {
+        const $submenuItems = $(SELECTORS.submenuToggle);
+
+        if (!$submenuItems.length) {
+            return;
+        }
+
+        $submenuItems.on('click', function (event) {
+            event.preventDefault();
+
+            const $toggle = $(this);
+            const $item = $toggle.parent();
+            const isOpen = $item.hasClass('is-open');
+
+            $item.siblings(SELECTORS.menuItemWithChildren)
+                .removeClass('is-open')
+                .children('.submenu-toggle')
+                .attr('aria-expanded', 'false');
+
+            $item.toggleClass('is-open', !isOpen);
+            $toggle.attr('aria-expanded', String(!isOpen));
+        });
+
+        $(document).on('click', function (event) {
+            if (!$(event.target).closest(SELECTORS.menuItemWithChildren).length) {
+                closeDesktopSubmenus();
+            }
+        });
+
+        $(document).on('keydown', function (event) {
             if (event.key === 'Escape') {
-                $('.menu-item-has-children.is-open')
-                    .removeClass('is-open')
-                    .children('.submenu-toggle')
-                    .attr('aria-expanded', 'false');
+                closeDesktopSubmenus();
             }
         });
     }
 
-    ///=============  * Menu Bar Popup Icon  =============\\\
-    function menuBarPopup() {
-        const menuTrigger = $('.menu__bar-trigger');
-        const menuPopup = $('.menu__bar-popup');
-        const menuOverlay = $('.menu__bar-popup-overlay');
+    function initializeMenuBarPopup() {
+        const $menuTrigger = $(SELECTORS.menuTrigger);
+        const $menuPopup = $(SELECTORS.menuPopup);
+        const $menuOverlay = $(SELECTORS.menuOverlay);
 
-        function closeMenu() {
-            menuTrigger.removeClass('clicked').attr('aria-expanded', 'false');
-            menuPopup.removeClass('show');
-            menuOverlay.removeClass('show');
+        if (!$menuTrigger.length || !$menuPopup.length || !$menuOverlay.length) {
+            return;
         }
 
-        menuTrigger.on("click", function() {
-            const isOpen = menuPopup.hasClass('show');
+        function closeMenu() {
+            $menuTrigger.removeClass('clicked').attr('aria-expanded', 'false');
+            $menuPopup.removeClass('show');
+            $menuOverlay.removeClass('show');
+        }
 
-            if (isOpen) {
+        $menuTrigger.on('click', function () {
+            if ($menuPopup.hasClass('show')) {
                 closeMenu();
                 return;
             }
 
-            menuTrigger.addClass('clicked').attr('aria-expanded', 'true');
-            menuPopup.addClass('show');
-            menuOverlay.addClass('show');
+            $menuTrigger.addClass('clicked').attr('aria-expanded', 'true');
+            $menuPopup.addClass('show');
+            $menuOverlay.addClass('show');
         });
 
-        $('.menu__bar-popup .close').on("click", function() {
-            closeMenu();
-        });
+        $('.menu__bar-popup .close').on('click', closeMenu);
+        $menuOverlay.on('click', closeMenu);
 
-        menuOverlay.on('click', function() {
-            closeMenu();
-        });
-
-        $(document).on('keydown', function(event) {
+        $(document).on('keydown', function (event) {
             if (event.key === 'Escape') {
                 closeMenu();
             }
         });
     }
 
-    ///=============  * Banner Slider  =============\\\
-    function bannerSlider() {
-        let sliderActive1 = '.banner-slider';
-        if (!document.querySelector(sliderActive1)) {
+    function initializeBannerSlider() {
+        const sliderSelector = SELECTORS.slider;
+
+        if (!document.querySelector(sliderSelector)) {
             return;
         }
 
-        let sliderInit1 = new Swiper(sliderActive1, {
+        const slider = new Swiper(sliderSelector, {
             loop: true,
             slidesPerView: 1,
             speed: 1000,
@@ -130,118 +157,125 @@
             navigation: {
                 nextEl: '.banner-button-next',
                 prevEl: '.banner-button-prev',
-            },		
+            },
             pagination: {
-                el: ".banner-pagination",
+                el: '.banner-pagination',
                 clickable: true,
             },
         });
-        function animated_swiper(selector, init) {
-            let animated = function animated() {
-                $(selector + ' [data-animation]').each(function() {
-                    let anim = $(this).data('animation');
-                    let delay = $(this).data('delay');
-                    let duration = $(this).data('duration');
-                    $(this).removeClass('anim' + anim).addClass(anim + ' animated').css({
+
+        function runSlideAnimations() {
+            $(sliderSelector + ' [data-animation]').each(function () {
+                const $element = $(this);
+                const animation = $element.data('animation');
+                const delay = $element.data('delay');
+                const duration = $element.data('duration');
+
+                $element
+                    .removeClass('anim' + animation)
+                    .addClass(animation + ' animated')
+                    .css({
                         webkitAnimationDelay: delay,
                         animationDelay: delay,
                         webkitAnimationDuration: duration,
-                        animationDuration: duration
-                    }).one('animationend', function() {
-                        $(this).removeClass(anim + ' animated');
+                        animationDuration: duration,
+                    })
+                    .one('animationend', function () {
+                        $(this).removeClass(animation + ' animated');
                     });
-                });
-            };
-            animated();
-            init.on('slideChange', function() {
-                $(sliderActive1 + ' [data-animation]').removeClass('animated');
             });
-            init.on('slideChange', animated);
         }
-        animated_swiper(sliderActive1, sliderInit1);
+
+        runSlideAnimations();
+
+        slider.on('slideChange', function () {
+            $(sliderSelector + ' [data-animation]').removeClass('animated');
+            runSlideAnimations();
+        });
     }
 
-    ///=============  Toggle contact fields based on selected service  =============\\\
     function updateServiceDependentFields() {
-        let serviceField = $('#service');
-        if (!serviceField.length) {
+        const $serviceField = $(SELECTORS.serviceField);
+
+        if (!$serviceField.length) {
             return;
         }
 
-        let selectedService = serviceField.val();
+        const selectedService = $serviceField.val();
 
-        $('.conditional-group').hide();
+        $(SELECTORS.conditionalGroup).each(function () {
+            const $group = $(this);
+            const servicesRaw = $group.data('service');
 
-        $('.conditional-group').each(function() {
-            let servicesRaw = $(this).data('service');
             if (!servicesRaw) {
+                $group.hide();
                 return;
             }
 
-            let services = servicesRaw.split(',').map(s => s.trim());
+            const services = String(servicesRaw)
+                .split(',')
+                .map((service) => service.trim());
 
-            services.includes(selectedService) ? $(this).show() : $(this).hide();
+            $group.toggle(services.includes(selectedService));
         });
     }
 
     function preselectServiceFromQuery() {
-        let serviceField = document.getElementById('service');
+        const serviceField = document.querySelector(SELECTORS.serviceField);
+
         if (!serviceField) {
             return;
         }
 
-        let selectedService = new URLSearchParams(window.location.search).get('service');
-        let allowedServices = ['entruempelung', 'entsorgung', 'aufloesung', 'umzug', 'transport'];
+        const selectedService = new URLSearchParams(window.location.search).get('service');
 
-        if (selectedService && allowedServices.includes(selectedService)) {
+        if (selectedService && ALLOWED_SERVICES.includes(selectedService)) {
             serviceField.value = selectedService;
         }
     }
 
-
-    function handleContactFormSubmit(event) {
-        if (typeof window.fetch !== 'function') {
+    function setMessage(element, message) {
+        if (!element) {
             return;
         }
 
-        event.preventDefault();
-
-        const myForm = event.target;
-        const submitButton = document.getElementById('form-submit-button');
-        const formError = document.getElementById('form-error');
-        const formStatus = document.getElementById('form-status');
-        const formSuccess = document.getElementById('form-success');
-
-        if (formError) {
-            formError.style.display = 'none';
-            formError.textContent = '';
+        if (!message) {
+            element.style.display = 'none';
+            element.textContent = '';
+            return;
         }
 
-        if (formSuccess) {
-            formSuccess.style.display = 'none';
+        element.style.display = 'block';
+        element.textContent = message;
+    }
+
+    function setSubmitButtonLoadingState(button, isLoading, defaultHtml) {
+        if (!button) {
+            return;
         }
 
-        if (formStatus) {
-            formStatus.style.display = 'block';
-            formStatus.textContent = 'Ihre Anfrage wird gesendet...';
+        button.disabled = isLoading;
+
+        if (isLoading) {
+            button.setAttribute('aria-disabled', 'true');
+            button.setAttribute('aria-busy', 'true');
+            button.textContent = 'Wird gesendet...';
+            return;
         }
 
-        let submitButtonHtml = '';
-        if (submitButton) {
-            submitButtonHtml = submitButton.innerHTML;
-            submitButton.disabled = true;
-            submitButton.setAttribute('aria-disabled', 'true');
-            submitButton.setAttribute('aria-busy', 'true');
-            submitButton.textContent = 'Wird gesendet...';
-        }
+        button.removeAttribute('aria-disabled');
+        button.removeAttribute('aria-busy');
+        button.innerHTML = defaultHtml;
+    }
 
-        const formData = new FormData(myForm);
+    function buildEncodedFormPayload(form) {
+        const formData = new FormData(form);
+
         if (!formData.get('form-name')) {
-            formData.set('form-name', myForm.getAttribute('name') || 'contact');
+            formData.set('form-name', form.getAttribute('name') || 'contact');
         }
 
-        const filteredEntries = Array.from(formData.entries())
-        .filter(([key, value]) => {
+        const filteredEntries = Array.from(formData.entries()).filter(([key, value]) => {
             if (key === 'form-name') {
                 return true;
             }
@@ -250,57 +284,73 @@
                 return true;
             }
 
-            return value.trim() !== "";
-        }); // only keep filled fields
-    
-        const cleanedFormData = new URLSearchParams(filteredEntries).toString();
-      
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: cleanedFormData
-        })
-          .then((response) => {
-            if (!response.ok) {
-                throw new Error('Request failed');
-            }
+            return value.trim() !== '';
+        });
 
-            myForm.style.display = 'none';
-            if (formStatus) {
-                formStatus.style.display = 'none';
-                formStatus.textContent = '';
-            }
-            if (formSuccess) {
-                formSuccess.style.display = 'block';
-            }
-          })
-          .catch(() => {
-            if (formStatus) {
-                formStatus.style.display = 'none';
-                formStatus.textContent = '';
-            }
-            if (formError) {
-                formError.style.display = 'block';
-                formError.textContent = 'Beim Senden Ihrer Anfrage ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut.';
-            }
-            if (submitButton) {
-                submitButton.disabled = false;
-                submitButton.removeAttribute('aria-disabled');
-                submitButton.removeAttribute('aria-busy');
-                submitButton.innerHTML = submitButtonHtml;
-            }
-          });
+        return new URLSearchParams(filteredEntries).toString();
     }
 
-    $(document).ready(function () {
+    function handleContactFormSubmit(event) {
+        if (typeof window.fetch !== 'function') {
+            return;
+        }
+
+        event.preventDefault();
+
+        const form = event.target;
+        const submitButton = document.getElementById('form-submit-button');
+        const formError = document.getElementById('form-error');
+        const formStatus = document.getElementById('form-status');
+        const formSuccess = document.getElementById('form-success');
+        const submitButtonHtml = submitButton ? submitButton.innerHTML : '';
+
+        setMessage(formError, '');
+
+        if (formSuccess) {
+            formSuccess.style.display = 'none';
+        }
+
+        setMessage(formStatus, 'Ihre Anfrage wird gesendet...');
+        setSubmitButtonLoadingState(submitButton, true, submitButtonHtml);
+
+        const payload = buildEncodedFormPayload(form);
+
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: payload,
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Request failed');
+                }
+
+                form.style.display = 'none';
+                setMessage(formStatus, '');
+
+                if (formSuccess) {
+                    formSuccess.style.display = 'block';
+                }
+            })
+            .catch(() => {
+                setMessage(formStatus, '');
+                setMessage(formError, 'Beim Senden Ihrer Anfrage ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut.');
+                setSubmitButtonLoadingState(submitButton, false, submitButtonHtml);
+            });
+    }
+
+    document.addEventListener('DOMContentLoaded', initializeMobileMenuClone);
+
+    $(function () {
+        initializeMobileMenuArrows();
+        initializeDesktopSubmenuToggle();
+        initializeMenuBarPopup();
+        initializeBannerSlider();
+
         preselectServiceFromQuery();
-        $('select#service').on('change', updateServiceDependentFields);
+        $(SELECTORS.serviceField).on('change', updateServiceDependentFields);
         updateServiceDependentFields();
 
-        $('#contact-form').on('submit', handleContactFormSubmit);
-
-        submenuToggle();
-        menuBarPopup();
-        bannerSlider();
+        $(SELECTORS.contactForm).on('submit', handleContactFormSubmit);
     });
 })(jQuery);
