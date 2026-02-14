d6d60980371c1035349ebb94bd1a5b521e3a300c
Nenad Gajic <nenad_gajic@outlook.com>
2026-02-14 16:23:16 +0100

    chore(s7-2): remove runtime jquery include and local asset

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -52,7 +52,6 @@
 
         <x-footer />
 
-        <script src="/assets/js/jquery-3.7.1.min.js"></script>
         <script src="/assets/js/bootstrap.min.js"></script>
         <script src="/assets/js/popper.min.js"></script>
         <script src="/assets/js/swiper-bundle.min.js"></script>
