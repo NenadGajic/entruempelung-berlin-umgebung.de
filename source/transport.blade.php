ff37e7eeba8e901f79c6a8d8bb85756e59e48f2b
Nenad Gajic <nenad_gajic@outlook.com>
2026-03-04 01:15:59 +0100

    feat(seo): add service json-ld on service pages

--- source/transport.blade.php
+++ source/transport.blade.php
@@ -3,6 +3,11 @@
     $title = $service['meta_title'] . ' | ' . ($page->titleSuffixInner ?? $page->title);
     $description = $service['meta_description'];
     $ogImage = $service['og_image'];
+    $serviceSchema = [
+        'name' => $service['h1'],
+        'description' => $service['meta_description'],
+        'image' => $service['og_image'] ?? ($service['hero']['src'] ?? null),
+    ];
 
     $breadcrumbs = [];
     foreach ($service['breadcrumbs'] as $crumb) {
